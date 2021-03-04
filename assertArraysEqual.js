0ba4c2846de893106ee4815fb909917d7aa65e3a
Penny Clark <respect.cactus@gmail.com>
2021-03-04 04:35:41 +0000

    fixed issue with eqArrays function and changed this function's language to be more reflective of the results

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,5 +1,8 @@
 const eqArrays = function(array1, array2) {
   let arrayMatch = true;
+  if (array1.length !== array2.length) {
+    arrayMatch = false;
+  }
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
       arrayMatch = false;
@@ -10,10 +13,12 @@ const eqArrays = function(array1, array2) {
 
 const assertArraysEqual = function(array1, array2) {
   eqArrays(array1, array2) ?
-    console.log(`✅😇Assertion passed: ${array1} === ${array2}`) :
-    console.log(`🙅🏻‍♀️❌Assertion failed: ${array1} !== ${array2}`);
+    console.log(`✅😇 These arrays are EQUAL ${array1} === ${array2}`) :
+    console.log(`🙅🏻‍♀️❌ Theses arrays are NOT EQUAL ${array1} !== ${array2}`);
 };
 
 //test cases
 assertArraysEqual([true, "hi"], [true, "hi"]); // => true
-assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // => not equal
+assertArraysEqual([1], []); // => not equal
+assertArraysEqual([1, 2], [1, 2, 3]) //=> not equal
